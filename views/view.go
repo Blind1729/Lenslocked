a53b2a01b25c264318dac36c92457b75627ca24c
Arun Suryan <2018PGCACA47@nitjsr.ac.in>
2022-06-27 16:07:32 +0530

    Refactor layout files parsing

--- views/view.go
+++ views/view.go
@@ -2,14 +2,20 @@ package views
 
 import (
 	"html/template"
+	"path/filepath"
+)
+
+const (
+	LayoutDir         = "views/layouts/"
+	TemplateExtension = ".gohtml"
 )
 
 func NewView(layout string, files ...string) *View {
-	// We append footer view to every page
-	files = append(files,
-		"views/layouts/footer.gohtml",
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml")
+	layoutFiles := layoutFiles()
+
+	for file := range layoutFiles {
+		files = append(files, layoutFiles[file])
+	}
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -25,4 +31,13 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+// Get all the layout files present under the layouts dir
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExtension)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
